5c5e0f179eb6056570bb79a401bda845aec7ac0e
Manish Singh <35570930+manishsingh7163@users.noreply.github.com>
2021-10-09 14:23:17 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Manish Singh',                   # Type in your name
   author_email = 'manishsingh7163@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/manishsingh7163/rasterlib',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/manishsingh7163/rasterlib/archive/refs/tags/rasterio.tar.gz',    # I explain this later on
   keywords = ['RASTER', 'RASTERDATA', 'ZONALSTATS'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'rasterio',
